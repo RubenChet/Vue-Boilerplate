9589f66574f029c4e17d3b2fb26640f816d4f8e0
RubenChet <rubenchetboun@gmail.com>
2023-02-13 00:20:59 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import Button from 'primevue/button';
+export default {
+  components: {
+    Button
+  }
+}
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  <Button label="Secondary" class="p-button-secondary"/>
+</template>
+
+<style scoped>
+</style>
